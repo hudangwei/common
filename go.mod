e26bb5e0b60beda5b87b690fd8b6d5a83e8bce5b
WOW\13131 <13131889@qq.com>
2023-11-26 10:57:11 +0800

    add parse

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.20
 
 require (
 	github.com/ClickHouse/clickhouse-go v1.5.4
+	github.com/astaxie/beego v1.12.3
 	github.com/didi/gendry v1.8.2
+	github.com/go-chi/chi v1.5.5
 	github.com/go-sql-driver/mysql v1.7.1
-	github.com/gomodule/redigo v1.8.9
+	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/syndtr/goleveldb v1.0.0
 	go.uber.org/zap v1.26.0
@@ -15,5 +17,6 @@ require (
 require (
 	github.com/cloudflare/golz4 v0.0.0-20150217214814-ef862a3cdc58 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 )
---
+++ parse/codec/multipart_form.go
@@ -0,0 +1,58 @@
+package codec
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+type MultipartForm struct {
+}
+
+func (j *MultipartForm) ContentType() []string {
+	return []string{"multipart/form-data"}
+}
+
+func (j *MultipartForm) UnmarshalSearchMap(req *http.Request) (SearchMap, error) {
+	// make request body could read multiple
+	bs, err := CopyBody(req)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := req.MultipartReader()
+	if err != nil {
+		return nil, fmt.Errorf("read form-data input from body failed: %s", err)
+	}
+
+	multiParts := SearchMap{}
+	for {
+		p, err := reader.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("read next part from body failed: %s", err)
+		}
+
+		bs, err := ioutil.ReadAll(p)
+		if err != nil {
+			return nil, fmt.Errorf("read part body from body failed: %s", err)
+		}
+		if p.FileName() != "" {
+			fmt.Println("multiparts filename", p.FileName(), p.FormName())
+			multiParts[fmt.Sprintf("%s%s", "_", p.FormName())] = []byte(p.FileName())
+		}
+
+		multiParts[p.FormName()] = bs
+	}
+
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(bs))
+	return multiParts, nil
+}
+
+func (j *MultipartForm) Marshal(ptr interface{}) ([]byte, error) {
+	panic("MultipartForm not support marshal")
+}
